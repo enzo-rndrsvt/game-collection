e92a45c50f8a06733b5c5c807c095d632ab77344
2Axelle9 <axelle.hannier@gmail.com>
2024-12-10 15:55:33 +0100

    view header

---
+++ src/contollers/header.php
@@ -0,0 +1,4 @@
+<?php
+    session_start();
+    $isUserLoggedIn = isset($_SESSION['userid']);
+?>
--- src/views/header.php
+++ src/views/header.php
@@ -1,45 +1,28 @@
-<?php
-    session_start();
-    $isUserLoggedIn = isset($_SESSION['userid']);
-    $isAdmin = isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] ;
-?>
-
-
 <!-- HEADER -->
 <header>
-    <a id="accueil" href="./index.php">
-        <img src="assets/logo.png" alt="Logo de l'ADIIL">
+    <a id="accueil" href="./home.php">
+        <img src="assets/images/logo.png" alt="Logo de de game collection">
     </a>
     <nav>
         <ul>
             <li>
-                <a href="./events.php">Événements</a>
+                <a href="./events.php">MA BIBLIOTHEQUE</a>
             </li>
             <li>
-                <a href="./news.php">Actualités</a>
+                <a href="/src/controllers/addAGame.php">AJOUTER UN JEU</a>
             </li>
             <li>
-                <a href="./shop.php">Boutique</a>
-            </li>
-            
-            <?php if ($isUserLoggedIn): ?>
-                <li>
-                    <a href="./agenda.php">Agenda</a>
-                </li>
-            <?php endif; ?>
-
-            <li>
-                <a href="./about.php">À propos</a>
+                <a href="/src/controllers/ranking.php">CLASSEMENT</a>
             </li>
 
             <?php if ($isUserLoggedIn): ?>
                 <li>
-                    <a href="./account.php">Mon compte</a>
+                    <a href=/src/controllers/profil.php>PROFIL</a>
                 </li>
 
             <?php else: ?>
                 <li>
-                    <a href="./login.php">Se connecter</a>
+                    <a href="/src/controllers/login.php">CONNEXION</a>
                 </li>
             <?php endif; ?>
 
