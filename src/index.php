b1c25494be2aed85280f27d001e8a4dc4a35d6dc
2Axelle9 <axelle.hannier@gmail.com>
2024-12-10 16:23:04 +0100

    Débuts page d'accueil et header

--- src/index.php
+++ src/index.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-
+require "views/home.php";
--- src/views/home.php
+++ src/views/home.php
@@ -6,13 +6,20 @@
     
     <title>Accueil</title>
     
-    <link rel="stylesheet" href="/src/assets/css/general.css">
-    <link rel="stylesheet" href="/src/assets/css/header.css">
-    <link rel="stylesheet" href="/src/assets/css/footer.css">
-    <link rel="stylesheet" href="/src/assets/css/home.css">
+    <link rel="stylesheet" href="assets/css/general.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/home.css">
 
 </head>
 <body>
-    
+
+    <!-- Importer les fichiers -->
+    <?php 
+        require_once "header.php" 
+    ?>
+
+    <h1>Ceci est l'accueil de game collection</h1>
+
 </body>
 </html>
