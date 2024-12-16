aa974256623d60495812e12f47ee69da088e7194
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-12-16 09:59:49 +0100

    Ajout du front de la page de connexion et d'inscription

---
+++ src/views/inscription.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="assets/css/general.css">
+    <link rel="stylesheet" href="assets/css/inscription.css">
+
+</head>
+
+<body>
+    <div class="connexion">
+        <h1>Inscription</h1>
+        <form method="post">
+            <div class="form">
+                <div>
+                    <p>Nom :</p>
+                    <input type="text" name="nom" />
+                </div>
+                <div>
+                    <p>Prénom :</p>
+                    <input type="text" name="prenom" />
+                </div>
+                <div>
+                    <p>Email :</p>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <p>Mot de passe :</p>
+                    <input type="password" name="password" />
+                </div>
+                <div>
+                    <p>Confirmation du mot de passe :</p>
+                    <input type="password" name="password" />
+                </div>
+                <div>
+                    <button type="submit">S'INSCRIRE</button>
+                </div>
+            </div>
+            <a href="">Se connecter</a>
+        </form>
+    </div>
+</body>
+
+</html>
