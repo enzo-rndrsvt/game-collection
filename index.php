e7d7429953e9487d0d503e8969eb09cacdd27e0c
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2024-12-18 00:17:38 +0100

    adding routes to login and register

--- index.php
+++ index.php
@@ -8,5 +8,22 @@ $dotenv->load();
 
 
 
+$request = $_SERVER['REQUEST_URI'];
+$basePath = "/";
 
+$route = str_replace($basePath, '', parse_url($request, PHP_URL_PATH));
+$route = trim($route, '/');
 
+$routes = [
+    'register' => 'src/views/register.php',
+    'login' => 'src/views/login.php',
+    '' => 'src/views/home.php',
+];
+
+if (array_key_exists($route, $routes)) {
+    require $routes[$route];
+} else {
+    // Page 404 si la route n'existe pas
+    http_response_code(404);
+    echo "404 - Page non trouvée";
+}
