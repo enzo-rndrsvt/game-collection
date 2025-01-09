cfbc30e05cac5edc76dd2d2de29ccfdb3073e420
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2025-01-09 19:58:41 +0100

    test

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// comments
-
 
 $basePath = dirname($_SERVER['SCRIPT_NAME']);
 $basePath = str_replace('\\', '/', $basePath);
