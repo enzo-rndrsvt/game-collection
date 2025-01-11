8a6d595b4868970d4485438fd122907cdd224c95
2Axelle9 <axelle.hannier@gmail.com>
2025-01-11 20:24:59 +0100

    message when game added without creation

--- src/controllers/addGame.php
+++ src/controllers/addGame.php
@@ -12,12 +12,13 @@ if (isset($_POST['game_id'])) {
     $user_id = $_SESSION['user_id'];
     $game_id = $_POST['game_id'];
     if(get_libraries($user_id, $game_id) > 0) {
-        $_SESSION['error'] = "Le jeu est déjà dans votre bibliothèque.";
+        $_SESSION['error'] = "⚠ Ce jeu est déjà dans votre bibliothèque.";
         header('Location: addGame');
         exit();
     }
 
     create_library($user_id, $game_id);
+    $_SESSION['validation'] = "✔ Jeu ajouté à votre bibliothèque avec succès !";
     header('Location: addGame');
     exit();
 }
